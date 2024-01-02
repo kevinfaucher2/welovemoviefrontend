941b3d739a32332ba40ccd011ca205418eefa2aa
Kevin <kfaucher@dtcc.edu>
2024-01-02 16:56:00 -0500

    package updates

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./shared/Header";
 import MoviesList from "./home/MoviesList";
 import DetailedMoviesList from "./movies/DetailedMoviesList";
 import FullMovie from "./movie/FullMovie";
 import TheaterList from "./theaters/TheaterList";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route exact path="/">
-          <MoviesList />
-        </Route>
-        <Route exact path="/movies">
-          <DetailedMoviesList />
-        </Route>
-        <Route exact path="/movies/:movieId">
-          <FullMovie />
-        </Route>
-        <Route exact path="/theaters">
-          <TheaterList />
-        </Route>
-      </Switch>
-    </Router>
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<MoviesList />} />
+          <Route exact path="/movies" element={<DetailedMoviesList />} />
+          <Route exact path="/movies/:movieId" element={<FullMovie />} />
+          <Route exact path="/theaters" element={<TheaterList />} />
+        </Routes>
+      </Router>
   );
 }
 
